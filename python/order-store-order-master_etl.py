0da964aea639e53bea9e0ffcfd3898f921c92ede
Krayulu <47818687+Krayulu@users.noreply.github.com>
2019-03-05 09:25:28 +0530

    Update order-store-order-master_etl.py

--- python/order-store-order-master_etl.py
+++ python/order-store-order-master_etl.py
@@ -38,6 +38,6 @@ df.createOrReplaceTempView("global_order_store_order_view")
 master = spark.sql("SELECT insert_timestamp, a.event_id, business_date, a.store_order_number,a.event_type,business_date_order_taken,tax_amount, discount_amount, subtotal_amount,  event_timestamp, makeline_print_date from (SELECT insert_timestamp, event_id, business_date, store_order_number,event_type,business_date_order_taken,tax_amount, discount_amount, subtotal_amount,  event_timestamp, makeline_print_date, ROW_NUMBER() OVER (PARTITION BY event_id, event_type ORDER BY event_timestamp DESC ) AS index from global_order_store_order_view ) a where a.index=1")
 
 #Writing output single file instead of creating mutiple out files
-master.coalesce(1).write.format('com.databricks.spark.csv').save(output,header = 'true')
+master.coalesce(1).write.format('com.databricks.spark.csv').save(output_path,header = 'true')
 #either obove line over below line can be used
-master.coalesce(1).write.csv(output,header = 'true')	
+master.coalesce(1).write.csv(output_path,header = 'true')	
